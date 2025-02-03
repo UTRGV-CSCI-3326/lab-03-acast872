0893da490f962b06ebdc0af44322a35f3d693ae9
acast872 <140276833+acast872@users.noreply.github.com>
2025-02-03 21:48:01 +0000

    Lab 3 changes

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,17 @@
+publlic class BankAccount
+{
+    publlic static void main(String[] args){
 
+        double myBank = 2175.37;
+        myBank = myBank-302.50;
+        myBank = myBank+50.03;
+        myBank = (myBank/2)+20;
+        myBank = myBank-1;
+        myBank = myBank*2;
+        myBank = myBank+1;
+
+        myBank = Math.round(myBank * 100.0) / 100.0;
+        System.out.println(String.format("%.2f", myBank));
+
+    }
+}
